ac875799cf8ca91b3daf8c52ba896eef8698aa87
K.B.Dharun Krishna <kbdharunkrishna@gmail.com>
2023-12-16 13:13:44 +0530

    feat,test: add plugin
    Signed-off-by: K.B.Dharun Krishna <kbdharunkrishna@gmail.com>

---
+++ plugin_test.go
@@ -0,0 +1,41 @@
+// Copyright 2023 - 2023, axtlos <axtlos@disroot.org>
+// Copyright 2023 - present, K.B.Dharun Krishna <mail@kbdharun.dev>
+// SPDX-License-Identifier: GPL-3.0-ONLY
+
+// Copyright 2023 - 2023, axtlos <axtlos@disroot.org>
+// SPDX-License-Identifier: GPL-3.0-ONLY
+
+package main
+
+import (
+	"testing"
+
+	"github.com/vanilla-os/vib/api"
+)
+
+type testModule struct {
+	Name       string
+	Type       string
+	ExtraFlags []string
+	Packages   []string
+}
+
+type testCases struct {
+	module   interface{}
+	expected string
+}
+
+var test = []testCases{
+	{testModule{"Single Package, Single Flag", "dnf", []string{"--verbose"}, []string{"bash"}}, "dnf install -y --verbose bash"},
+	{testModule{"Single Package, No Flag", "dnf", []string{""}, []string{"bash"}}, "dnf install -y bash"},
+	{testModule{"Multiple Packages, No Flag", "dnf", []string{""}, []string{"bash", "fish"}}, "dnf install -y bash fish"},
+	{testModule{"Multiple Packages, Multiple Flags", "dnf", []string{"--verbose", "-y"}, []string{"bash", "fish"}}, "dnf install -y --verbose bash fish"},
+}
+
+func TestBuildModule(t *testing.T) {
+	for _, testCase := range test {
+		if output, _ := BuildModule(testCase.module, &api.Recipe{}); output != testCase.expected {
+			t.Errorf("Output %q not equivalent to expected %q", output, testCase.expected)
+		}
+	}
+}
